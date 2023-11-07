1854b82d8193b8fc9d3c51b2f7f502df6d1890e8
crb <cbanger@sl.on.ca>
2023-11-07 11:44:46 -0500

    Update Spring Boot Version

--- src/main/java/comp31/cruddemo/controllers/BookController.java
+++ src/main/java/comp31/cruddemo/controllers/BookController.java
@@ -2,7 +2,6 @@ package comp31.cruddemo.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,50 +9,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import comp31.cruddemo.services.BookService;
 
-
 @Controller
 public class BookController {
-    
+
     BookService bookService;
 
-    @Autowired
     public BookController(BookService bookService) {
         super();
         this.bookService = bookService;
     }
 
     Logger logger = LoggerFactory.getLogger(BookController.class);
-    
+
     @GetMapping("/")
-    public String getIndex()
-    {
-            return "index";
+    public String getIndex() {
+        return "index";
     }
 
     @GetMapping("/books")
     public String getBooks(
-        @RequestParam(required=false) String firstName, 
-        @RequestParam(required=false) String lastName, 
-        Model model)
-    {
-        boolean authorNameInvalid = 
-            firstName == null || firstName.isEmpty() || 
-            lastName  == null || lastName.isEmpty();
-        
-        if (authorNameInvalid)
-        {   model.addAttribute("books", bookService.findBooks());
-        }
-        else
-        {   logger.info("Author Name: ", firstName + " " + lastName);
-            model.addAttribute("books", bookService.findBooksByAuthor(firstName,lastName));
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            Model model) {
+        boolean authorNameInvalid = firstName == null || firstName.isEmpty() ||
+                lastName == null || lastName.isEmpty();
+
+        if (authorNameInvalid) {
+            model.addAttribute("books", bookService.findBooks());
+        } else {
+            logger.info("Author Name: ", firstName + " " + lastName);
+            model.addAttribute("books", bookService.findBooksByAuthor(firstName, lastName));
         }
         return "books";
     }
 
     @GetMapping("/authors")
-    public String getAuthors(Model model)
-    {
-        model.addAttribute("authors" , bookService.findAuthors());
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", bookService.findAuthors());
         return "authors";
     }
 
--- src/main/java/comp31/cruddemo/services/BookService.java
+++ src/main/java/comp31/cruddemo/services/BookService.java
@@ -3,7 +3,6 @@ package comp31.cruddemo.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import comp31.cruddemo.model.entities.Author;
@@ -13,37 +12,31 @@ import comp31.cruddemo.model.repositories.BookRepo;
 
 @Service
 public class BookService {
-    
+
     BookRepo bookRepo;
     AuthorRepo authorRepo;
-    
-    @Autowired
-    public BookService (BookRepo bookRepo, AuthorRepo authorRepo) {
+
+    public BookService(BookRepo bookRepo, AuthorRepo authorRepo) {
         super();
         this.bookRepo = bookRepo;
         this.authorRepo = authorRepo;
     }
 
-
-    public Iterable<Author> findAuthors()
-    {
+    public Iterable<Author> findAuthors() {
         return authorRepo.findAll();
     }
 
-    public Iterable<Book> findBooks()
-    {
+    public Iterable<Book> findBooks() {
         return bookRepo.findAll();
     }
 
-    public Iterable<Book> findBooksByAuthor(String firstName, String lastName)
-    {        
-        List <Book> bookList = new ArrayList<>();
-        List<Author> authorList = authorRepo.findByFirstNameIgnoreCaseAndLastNameIgnoreCase(firstName,lastName);
+    public Iterable<Book> findBooksByAuthor(String firstName, String lastName) {
+        List<Book> bookList = new ArrayList<>();
+        List<Author> authorList = authorRepo.findByFirstNameIgnoreCaseAndLastNameIgnoreCase(firstName, lastName);
         // List<Author> authorList = authorRepo.findByLastNameIgnoreCase(lastName);
-        if (!authorList.isEmpty())
-        {
+        if (!authorList.isEmpty()) {
             Author author = authorList.get(0);
-            bookList = author.getBooks();            
+            bookList = author.getBooks();
         }
         return bookList;
     }
